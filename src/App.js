ebad9b90b8cecebe948f2110d9db2bfcd41f4614
D5-80505-RutvikKumbhar <rutvikkumbhar@gmail.com>
2024-01-01 00:10:18 +0530

    added components in project

--- src/App.js
+++ src/App.js
@@ -17,7 +17,6 @@
 //         >
 //           Learn React
 //         </a>
-//         <h1>This is RTOMS</h1>
 //       </header>
 //     </div>
 //   );
@@ -27,38 +26,49 @@
 
 import React from 'react';
 import './App.css';
+
+
+// import { Header, Footer, Navbar } from './Components/';
 import Header from './Components/Header';
 import Footer from './Components/Footer';
+import Navbar from './Components/Navbar';
 
 function App() {
   return (
     <div className="App">
-        <div id="header">
-          <Header />
-        </div>
-      <nav>
-        <ul>
-          <li>About us</li>
-          <li>Online Services</li>
-          <li>Information Services</li>
-          <li>External Links</li>
-          <li>Contact Us</li>
-          <li>Login</li>
-        </ul>
-      </nav>
+      <div id="header">
+        <Header />
+      </div>
+      <div id="navbar">
+        <Navbar />
+      </div>
       <main className="App-body">
         <div className="ads">Ads</div>
-        <div className="services">
-          <div className="card">Drivers/ Learners License</div>
-          <div className="card">RTOMS</div>
-          <div className="card">Online Test/ Appointment</div>
-          <div className="card">Ads</div>
-          <div className="card">Vehicle Registration</div>
+        <div className="container">
+
+
+          {/* <div class="card">
+            <img src="..." class="card-img-top" alt="..."/>
+              <div class="card-body">
+                <h5 class="card-title">Card title</h5>
+                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                <a href="#" class="btn btn-primary">Apply Here</a>
+              </div>
+          </div> */}
+
+
+
+
+          <div className="box">Drivers/ Learners License</div>
+          <div className="box">RTOMS</div>
+          <div className="box">Online Test/ Appointment</div>
+          {/* <div className="box">Ads</div> */}
+          <div className="box">Vehicle Registration</div>
         </div>
       </main>
-        <div id="footer">
-          <Footer />
-        </div>
+      <div id="footer">
+        <Footer />
+      </div>
     </div>
   );
 }
---
+++ src/Components/Navbar.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+const Navbar = () => {
+  return (
+    <nav>
+        <ul>
+          <li>About us</li>
+          <li>Online Services</li>
+          <li>Information Services</li>
+          <li>External Links</li>
+          <li>Contact Us</li>
+          <li>Login</li>
+        </ul>
+      </nav>
+  );
+}
+
+export default Navbar;
